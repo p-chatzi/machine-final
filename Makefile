82cd52db9edc34bae12f0e4cee29194c08ab1c06
p-chatzi <peter.ace16@gmail.com>
2024-11-08 14:03:15 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+#Variables
+FLAGS := -Wall -Werror
+
+default: run
+
+setup:
+	mkdir -p out
+
+clean:
+	rm -rf out
+
+build: setup
+	gcc $(FLAGS) src/*.c -o out/chess
+
+run: build
+	./out/chess
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // printf()
+#include <stdlib.h> // atoi()
+#include <stdbool.h> // bool
+#include <string.h> // strcmp()
+
+#include "main.h"
+
+int main(){
+
+    
+    return 0;
+}
